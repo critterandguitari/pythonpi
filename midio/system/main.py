9f21f14497918407736b06a3a6749b4c6a922025
owen osborn <owen@critterandguitari.com>
2013-01-22 22:38:49 +0000

    basic structure

--- midio/system/main.py
+++ midio/system/main.py
@@ -14,8 +14,30 @@ serialport = serial.Serial("/dev/ttyAMA0", 38400)
 
 
 # Create an instance of the PyScope class
-fb = fullfb.init()
-print "opening fb"
+screen = fullfb.init()
+
+print "opening frame buffer"
+
+
+print "loading patches"
+import imp
+patches = []
+patches.append(imp.load_source('p1','../patches/p1.py'))
+patches.append(imp.load_source('p2','../patches/p2.py'))
+
+
+count = 0
+while 1:
+
+    count += 1
+    if count > 30 : count = 0
+    print count
+    
+    patches[0].loop(screen)
+
+    pygame.display.flip()
+
+
 
 time.sleep(1)
 
