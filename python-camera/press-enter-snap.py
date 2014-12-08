d42cca9139e21994eb78d7d580ebfc855e5a2534
oweno <owen@critterandguitari.com>
2014-12-08 01:54:22 +0000

    press enter for snapshot

--- python-camera/press-enter-snap.py
+++ python-camera/press-enter-snap.py
@@ -7,6 +7,6 @@ with picamera.PiCamera() as camera:
     count = 1
     while True :
         raw_input("press enter take pic")
-        camera.capture("../jpegs/" + str(count) + ".jpg")
+        camera.capture("./jpegs/" + str(count) + ".jpg", use_video_port=True)
         count += 1
          
